fad9d2b9579dd6d8a32e2a72c29b2667d5fb0460
moody ali <nadoo.ali.15.4@gmail.com>
2016-11-05 18:33:59 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -175,5 +175,5 @@ echo ""
     rm gban1
     
   fi
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./DevTSHAKE/DevTSHAKE.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./TSHAKEDEV/TSHAKEDEV.lua -l 1 -E $@
 fi
