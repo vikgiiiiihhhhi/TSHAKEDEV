20c745ea9187b3c28d44c7a82f141c54789c7c48
moody ali <nadoo.ali.15.4@gmail.com>
2016-11-05 18:41:18 +0300

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -24,13 +24,13 @@ local function pre_process(msg)
 local function moody(msg, matches)
     chat_id =  msg.to.id
     
-    if is_momod(msg) and matches[1] == 'قفل اعاده توجيه' or matches[1] == "c fd"  then
+    if is_momod(msg) and matches[1] == "c fd"  then
       
             
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
                     return ""
-  elseif is_momod(msg) and matches[1] == 'فتح اعاده توجيه' or matches[1] == "o fd"  then
+  elseif is_momod(msg) and matches[1] == "o fd"  then
       local hash = 'mate:'..msg.to.id
       redis:del(hash)
       return ""
@@ -40,10 +40,6 @@ end
 
 return {
     patterns = {
-        '^(قفل اعاده توجيه)$', 
-        '^(فتح اعاده توجيه)$',
-        '^[/!#](قفل اعاده توجيه)$', 
-        '^[/!#](فتح اعاده توجيه)$',
         '^(c fd)$', 
         '^(o fd)$',
         '^[/!#](c fd)$', 
